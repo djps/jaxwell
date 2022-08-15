a15fb7ffd79f2c2c8160cb7218872c01af0647dd
David Sinden <djps@users.noreply.github.com>
2022-08-15 14:49:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
-import setuptools
+import io
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("jwave", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open( os.path.join(os.path.dirname(__file__), *paths), encoding=kwargs.get("encoding", "utf8"), ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    """gets requirements"""
+    return [line.strip() for line in read(path).split("\n")  if not line.startswith(('"', "#", "-", "git+"))]
+
 
 setuptools.setup(
     name='jaxwell',
